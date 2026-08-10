b8c4bb395d91cdf73616d6301b6b6ff95b0b04e1
Huỳnh Hoàng Minh Quân <alphatcx123@gmail.com>
2026-08-10 12:41:25 +0700

    Delete settings-sync.js

--- settings-sync.js
+++
@@ -1,9 +0,0 @@
-(function(){
-try{
-window.ATCXSettings={
-key:"atcx_settings",
-load:function(){return JSON.parse(localStorage.getItem(this.key)||"{}")},
-save:function(v){localStorage.setItem(this.key,JSON.stringify(v))}
-};
-}catch(e){}
-})();
