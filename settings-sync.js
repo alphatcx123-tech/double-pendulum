96dafc7afb02e4e5c1554597f24b0564a75a7b8b
Huỳnh Hoàng Minh Quân <alphatcx123@gmail.com>
2026-08-10 12:05:14 +0700

    Update settings-sync.js

--- settings-sync.js
+++ settings-sync.js
@@ -1,1 +1,9 @@
-(function(){try{window.ATCXSettings={key:"atcx_settings",load:function(){return JSON.parse(localStorage.getItem(this.key)||"{}")},save:function(v){localStorage.setItem(this.key,JSON.stringify(v))}}}catch(e){}})();
+(function(){
+try{
+window.ATCXSettings={
+key:"atcx_settings",
+load:function(){return JSON.parse(localStorage.getItem(this.key)||"{}")},
+save:function(v){localStorage.setItem(this.key,JSON.stringify(v))}
+};
+}catch(e){}
+})();
